c5de2808e023f703b8faaf67e038b29ff63a72e4
Faiz Shakoor <121912022+fshakoor@users.noreply.github.com>
2024-01-02 14:44:55 -0600

    Logic written to check for draw and function to check if gameboard array is full

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Gameboard = {
     ],
 
     curPlayer: 'X',
+    gameOver: false,
 
     topLeft: document.querySelector('.upper-left'),
     topMid: document.querySelector('.upper-mid'),
@@ -40,70 +41,71 @@ const Gameboard = {
         };
     },
 
+    checkFull: function(gameBoard) {
+        for (let row = 0; row < gameBoard.length; row++) {
+            if (gameBoard[row].some(cell => cell === '')) {
+                return false;
+            }
+        }
+        return true;
+    },
+
     checkWin: function() {
         if (Gameboard.gameBoard[0][0] == Gameboard.gameBoard[0][1] && Gameboard.gameBoard[0][0] == Gameboard.gameBoard[0][2] && Gameboard.gameBoard[0][0] !== '') {
-            console.log('W')
+            console.log('W');
+            this.gameOver = true;
+            console.log(this.gameOver)
         }
         if (Gameboard.gameBoard[1][0] == Gameboard.gameBoard[1][1] && Gameboard.gameBoard[1][0] == Gameboard.gameBoard[1][2] && Gameboard.gameBoard[1][0] !== '') {
-            console.log('W')
+            console.log('W');
+            this.gameOver = true;
+            console.log(this.gameOver)
         }
         if (Gameboard.gameBoard[2][0] == Gameboard.gameBoard[2][1] && Gameboard.gameBoard[2][0] == Gameboard.gameBoard[2][2] && Gameboard.gameBoard[2][0] !== '') {
-            console.log('W')
+            console.log('W');
+            this.gameOver = true;
+            console.log(this.gameOver)
         }
         if (Gameboard.gameBoard[0][0] == Gameboard.gameBoard[1][0] && Gameboard.gameBoard[0][0] == Gameboard.gameBoard[2][0] && Gameboard.gameBoard[0][0] !== '') {
-            console.log('W')
+            console.log('W');
+            this.gameOver = true;
+            console.log(this.gameOver)
         }
         if (Gameboard.gameBoard[0][1] == Gameboard.gameBoard[1][1] && Gameboard.gameBoard[0][1] == Gameboard.gameBoard[2][1] && Gameboard.gameBoard[0][1] !== '') {
-            console.log('W')
+            console.log('W');
+            this.gameOver = true;
+            console.log(this.gameOver)
         }
         if (Gameboard.gameBoard[0][2] == Gameboard.gameBoard[1][2] && Gameboard.gameBoard[0][2] == Gameboard.gameBoard[2][2] && Gameboard.gameBoard[0][2] !== '') {
-            console.log('W')
+            console.log('W');
+            this.gameOver = true;
+            console.log(this.gameOver)
         }
         if (Gameboard.gameBoard[0][0] == Gameboard.gameBoard[1][1] && Gameboard.gameBoard[0][0] == Gameboard.gameBoard[2][2] && Gameboard.gameBoard[0][0] !== '') {
-            console.log('W')
+            console.log('W');
+            this.gameOver = true;
+            console.log(this.gameOver)
         }
         if (Gameboard.gameBoard[0][2] == Gameboard.gameBoard[1][1] && Gameboard.gameBoard[0][2] == Gameboard.gameBoard[2][0] && Gameboard.gameBoard[0][2] !== '') {
-            console.log('W')
+            console.log('W');
+            this.gameOver = true;
+            console.log(this.gameOver)
         }
+
+        if (this.checkFull(this.gameBoard) === true && this.gameOver === false) {console.log('draw')}
     }
 };
 
 Gameboard.init();
 
-let consoleCheck = document.querySelector('.check-console')
-consoleCheck.addEventListener("click", () => {
-    for (i = 0; i<3; i++) {
-        console.log(Gameboard.gameBoard[i])
-    }
-})
+// TODO: Show who's move it is
 
-// TODO: check for draw
+// TODO: Styling
 
-// TODO: Try tucking as much as you can inside factories. If you only need a single 
-// instance of something (e.g. the gameboard, the displayController etc.) then 
-// wrap the factory inside an IIFE (module pattern) so it cannot be reused
-// to create additional instances.
+// TODO: Draw line on 3 in a row
 
-// TODO:  think carefully about where each bit of logic should reside. 
-// Each little piece of functionality should be able to fit in the game,
-// player or gameboard objects.
+// TODO: Clean up the interface to allow players to put in their names
 
-// LOGIC/PARTS/TODO:
-// Clicking a div/tile adds an X or O to the tile AND to the gameboard array
-// Logic to check for win
-// Show who's move it is
+// TODO: include a button to start/restart the game
 
-// TODO: Focus on getting a working game in the console first before styling/DOM
-
-// TODO: Once you have a working console game, create an object that will handle 
-// the display/DOM logic. Write a function that will render the contents of the gameboard 
-// array to the webpage (for now, you can always just fill the gameboard array 
-// with "X"s and "O"s just to see what’s going on).
-
-// TODO: Write the functions that allow players to add marks to a specific spot on 
-// the board by interacting with the appropriate DOM elements (e.g. letting players 
-// click on a board square to place their marker). Don’t forget the logic that keeps 
-// players from playing in spots that are already taken!
-
-// TODO: Clean up the interface to allow players to put in their names, include a button
-// to start/restart the game and add a display element that shows the results upon game end!
+// TODO: add a display element that shows the results upon game end!
